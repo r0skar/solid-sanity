6929fc4d96e1e24dbe060f071d809cc6d0ae7e15
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-22 15:22:13 +0100

    create base App component

---
+++ web/src/App.tsx
@@ -0,0 +1,1 @@
+export const App: Solid.FC = () => <>App</>
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,12 +1,12 @@
 import { render } from 'solid-js/dom'
 import { forceRenderStyles } from 'typestyle'
 import { createGlobalStyles } from './design'
+import { App } from './App'
 
 const anchor = document.querySelector('#app')
-const Root = () => <div>App</div>
 
 if (anchor) {
   createGlobalStyles()
   forceRenderStyles()
-  render(Root, anchor)
+  render(() => <App />, anchor)
 }
